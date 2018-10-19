893ffffe4a764e05f2b2da6cbe12c004997f446b
stbischof <stbischof@bipolis.org>
2018-10-20 00:01:32 +0200

    [docker] update bnd docker image, use jdk to enable compile
    Signed-off-by: stbischof <stbischof@bipolis.org>

--- docker/build.sh
+++ docker/build.sh
@@ -2,20 +2,25 @@
 set -e
 
 # Edit these properties when moving to a new version.
-VERSION=3.3.0
+VERSION=4.1.0
 # SHA-256...
-SHA=b6b68dfcd0f5ba767a202bf35eb3eb964c63e679e8217dd514dac807e6cedee8
+SHA=5ad18a5a8702fcfe42d5180ee4ba33cf4728d5f68055b2d3755a86222afac462
 
 mkdir -p tmp
 if [ ! -f tmp/bnd.jar ]; then
 	echo Downloading bnd JAR version $VERSION
-	curl https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/$VERSION/biz.aQute.bnd-$VERSION.jar > tmp/bnd.jar
+        curl https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/$VERSION/biz.aQute.bnd-$VERSION.jar > tmp/bnd.jar
 else
 	echo Already downloaded bnd JAR version $VERSION. If SHA check fails, try deleting tmp folder.
 fi
 
 echo Checking SHA...
 echo "$SHA  tmp/bnd.jar" > tmp/checksum
+
+# install shasum
+# yum install -y perl-Digest-SHA
+# dnf install perl-Digest-SHA
+
 shasum -a 256 -c tmp/checksum
 
 echo Building Docker image
